a76f6246e85f3dcf753f277154f5cb69876c6e0d
sandipsawant10 <sandipsawant503@gmail.com>
2026-02-13 23:06:32 +0530

    Add initial server setup and package dependencies
    - Created server.js with basic Express setup
    - Updated package.json to include nodemon for development
    - Added new controller and route files for future implementation

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req,res) => {
+    res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
